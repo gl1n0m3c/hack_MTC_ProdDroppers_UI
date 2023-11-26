1b0f078a2c96a80aa0971304edb82552961ef8d9
Juonior <gogame37@gmail.com>
2023-11-26 07:35:54 +0300

    js to profile

--- proddroppers_UI/static/js/profile/scripts.js
+++ proddroppers_UI/static/js/profile/scripts.js
@@ -3,3 +3,28 @@ var currentUrl = window.location.href;
 // Извлекаем цифры из URL
 var userid = currentUrl.match(/\d+/)[0];
 alert(userid)
+
+xhr.open('GET', 'https://music-mts.ru:5000/users/profile/'+userid+'/', true);
+
+// Установка обработчика события загрузки
+xhr.onload = function () {
+  if (xhr.status >= 200 && xhr.status < 300) {
+    // Обработка данных в формате JSON
+    var responseData = JSON.parse(xhr.responseText);
+    console.log(responseData);
+    var usernameHeadings = document.querySelectorAll('[id="username"]');
+    usernameHeadings.forEach(function (heading) {
+        heading.textContent = responseData.username; // Замените "Новое значение" на ваше новое значение
+    });
+    var emnailHeadings = document.querySelectorAll('[id="email"]');
+    emnailHeadings.forEach(function (heading) {
+        heading.textContent = responseData.email; // Замените "Новое значение" на ваше новое значение
+    });
+  } else {
+    // Обработка ошибок
+    alert('There was a problem with the request:', xhr.statusText);
+  }
+};
+
+// Отправка запроса
+xhr.send();
