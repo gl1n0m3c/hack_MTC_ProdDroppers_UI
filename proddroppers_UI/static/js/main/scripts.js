4af74dde9464523eecbf9aa57c085dd4b0687551
Juonior <gogame37@gmail.com>
2023-11-26 07:23:33 +0300

    add js to navbar

--- proddroppers_UI/static/js/main/scripts.js
+++ proddroppers_UI/static/js/main/scripts.js
@@ -1,5 +1,9 @@
 
 userid = sessionStorage.getItem('userid');
+var library = document.getElementById('library')
+var profile = document.getElementById('profile')
+library.href = "music/"+userid
+profile.href = "profile/"+userid
 var xhr = new XMLHttpRequest();
 
 // Настройка запроса (GET-запрос по указанному URL)
